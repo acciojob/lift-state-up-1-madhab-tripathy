9699e79da175d90c4fc2e1df1c6de91baf95fa95
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-08-14 14:00:50 +0530

    2nd commit

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,12 +2,20 @@ import React,{useState} from "react";
 
 const Parent = ()=>{
     const [showModal,setShowModal] = useState(false);
+    function modalChange(){
+        document.getElementById('box').style.display = 'block';
+        setShowModal(true)
+    }
     return(
         <div className="parent">
             <h1>Parent Component</h1>
             <div className="child">
                 <h2>Child Component</h2>
-                <button onClick={()=>{setShowModal(true)}}>Show Modal</button>
+                <button onClick={modalChange}>Show Modal</button>
+                <div id="box">
+                    <h3>Modal Content</h3>
+                    <p>This is the modal content.</p>
+                </div>
             </div>
         </div>
     );
